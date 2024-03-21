9c2d0b0adcdc900d151dde5dd7ce69ea9556b1df
motkvil-dev <motkvil@gmail.com>
2024-03-21 13:44:07 +0100

    update iu three js

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
+import { Box } from '@mui/material'
+import { Canvas, useFrame } from '@react-three/fiber'
+import MyMesh from './components/iuMesh'
 import './App.css'
 
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(true)
+  
+  useEffect(()=>{
+    if(open){
+      gsap.to('.box2',{
+        height:'100vh',
+        duration:1
+      })
+      
+    }else{
+      
+      gsap.to('.box2',{
+        height:0,
+        duration:1
+      })
+    }
+  },[open])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box
+      position={'relative'}
+      bgcolor={'gray'}
+      width={'100vw'}
+      height={'100vh'}
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      overflow={'hidden'}
+    >
+      <Box
+        width={'100vw'}
+        className='box2'
+        bgcolor={'black'}
+        position={'absolute'}
+        color={'white'}
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'100vh'}
+        overflow={'hidden'}
+        top={0}
+      />
+
+      <Box
+        position={'absolute'}
+        left={0} 
+        height={'100vh'}
+        width={'100vw'}
+        className='iuBox'
+      >
+        <Canvas>
+          <hemisphereLight
+            groundColor={'red'}
+            color={'yellow'}
+          />
+
+          <MyMesh setOpen={setOpen} open={open} position={[0,0,0]} />
+          <MyMesh setOpen={setOpen} open={open} position={[0,0,0]} />
+
+        </Canvas>
+      </Box>
+
+      <Box
+        width={'100vw'}
+        className='box2'
+        position={'absolute'}
+        color={'white'}
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'100vh'}
+        overflow={'hidden'}
+        top={0}
+      >
+        <Box paddingBottom={5} className='fontGlass'
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          flexDirection={'column'}
+        >
+          <Box 
+            fontWeight={400} 
+            fontSize={90} 
+            textAlign={'center'} 
+            pb={2} onClick={()=>setOpen(!open)}
+          >
+            <Box>DEL-UX</Box>
+          </Box>
+          <Box fontSize={20} textAlign={'left'}>
+            <Box>☉ Diseño UX/UI</Box>
+            <Box>☉ Diseño Gráfico</Box>
+            <Box>☉ Programación web</Box>
+          </Box>
+        </Box>
+      
+      </Box>
+
+    </Box>
   )
 }
 
---
+++ src/components/iuMesh.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
+import { useFrame } from '@react-three/fiber'
+
+
+export default function MyMesh (props){
+    const uiRef = useRef()
+  
+    useFrame(({clock, camera})=>{
+      uiRef.current.rotation.x = clock.getElapsedTime() * 0.1
+      uiRef.current.rotation.z = clock.getElapsedTime() * 0.1
+
+      if(props.open){
+        gsap.to(camera.position,{
+            z:10, x:0,
+            duration:1,
+        })
+      } else {
+        gsap.to(camera.position,{
+            z:10, x:1,
+            duration:1,
+        })
+      }
+    })
+  
+    return (
+      <mesh position={props.position} ref={uiRef} onClick={()=>props.setOpen(!props.open)} >
+        <sphereGeometry args={[2,20,20]}/>
+        <meshBasicMaterial flatShading wireframe/>
+      </mesh>
+    )
+  }
+
