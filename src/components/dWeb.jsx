c01c58298526deb3f89172e0581dede902687e17
motkvil-dev <motkvil@gmail.com>
2024-04-02 12:48:20 +0200

    UI Update

---
+++ src/components/dWeb.jsx
@@ -0,0 +1,37 @@
+import { Box } from "@mui/material"
+
+const DWeb = () => {
+
+return(
+<Box>
+ <Box
+  className='fontGlass'
+  padding={1} fontSize={50}
+ >
+  Desarrollo WEB
+ </Box>
+
+ <Box
+  padding={1}
+  className='fontMontserrat'
+ >
+  Nuestro servicio de diseño web ofrece soluciones 
+  personalizadas para crear sitios web modernos y 
+  funcionales que cautivan a tu audiencia desde el 
+  primer clic. Desde el diseño de interfaces 
+  elegantes hasta la optimización de la experiencia 
+  del usuario, nuestro equipo experto fusiona la 
+  creatividad con la funcionalidad para ofrecerte 
+  un sitio web que no solo se ve impresionante, 
+  sino que también cumple con tus objetivos 
+  comerciales. Con atención al detalle y enfoque 
+  en la usabilidad, creamos experiencias digitales 
+  que destacan en un mercado en constante evolución 
+  y garantizan una conexión duradera con tus usuarios.
+ </Box>
+</Box>
+)
+
+}
+
+export default DWeb
---
+++ src/components/duxui.jsx
@@ -0,0 +1,30 @@
+import { Box } from "@mui/material"
+
+const DUXUI = () => {
+
+    return(
+        <Box>
+            <Box
+                className='fontGlass'
+                padding={1} fontSize={50}
+            >
+                Diseño UX/UI
+            </Box>
+
+            <Box
+                padding={1}
+                className='fontMontserrat'
+            >
+                Nuestro servicio de diseño UX/UI se centra en crear 
+                experiencias digitales atractivas y fáciles de usar. 
+                Desde interfaces intuitivas hasta diseños visualmente 
+                impactantes, fusionamos la estética con la 
+                funcionalidad para que tu producto destaque y cautive 
+                a tus usuarios desde el primer momento.
+            </Box>
+        </Box>
+    )
+
+}
+
+export default DUXUI
